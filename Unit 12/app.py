8fcc243b96eb94d63fdba87c42f3bb2e004d7b4c
fluffysheepy <81768104+fluffysheepy@users.noreply.github.com>
2022-05-02 19:41:09 +0300

    Add files via upload

--- Unit 12/app.py
+++ Unit 12/app.py
@@ -7,3 +7,11 @@ app = Flask(__name__)
 def suppliers():
     suppliers = database.get_suppliers()
     return render_template('index.html', suppliers=suppliers)
+
+
+@app.route("/suppliers/<int:supplier_id>")
+def products(supplier_id):
+    products = database.get_supplier_products(supplier_id)
+    suppliers = database.get_suppliers()
+    companynames = database.get_companyname(supplier_id)
+    return render_template("products.html", products=products, suppliers=suppliers, companynames=companynames)
--- Unit 12/database.py
+++ Unit 12/database.py
@@ -1,9 +1,32 @@
 import sqlite3
 
-def get_suppliers():
+def query(query_text, *param):
     conn = sqlite3.connect('Northwind_large.sqlite')
     cur = conn.cursor()
-    cur.execute("""SELECT CompanyName, City, Country FROM Supplier""")
+    cur.execute(query_text, param)
+    
+    column_names=[]
+    for column in cur.description:
+        column_names.append(column[0])
+
     rows = cur.fetchall()
+    dicts=[]
+
+    for row in rows:
+        d=dict(zip(column_names, row))
+        dicts.append(d)
+
     conn.close()
-    return rows
+    return dicts
+
+def get_suppliers():
+    return query("""SELECT * FROM Supplier""")
+
+def get_supplier_products(supplier_id):
+    return query("""
+    SELECT ProductName, QuantityPerUnit, UnitPrice FROM Product
+    WHERE SupplierId = ?""", supplier_id)
+
+def get_companyname(supplier_id):
+    return query("""
+    SELECT CompanyName FROM Supplier WHERE Id=?""", supplier_id)
